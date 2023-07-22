df8cf798667187cfc3839d2dc0113b13a7403584
MashroorNitol <43212455+MashoorNitol@users.noreply.github.com>
2023-07-21 22:24:11 -0600

    Update thermal_data.py

--- vasp_data_generate/thermal_data.py
+++ vasp_data_generate/thermal_data.py
@@ -1,31 +1,32 @@
 import os
-import shutil
+import random
 import numpy as np
+import shutil
 
-def vec2angle(vec1, vec2):
-    angle = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
-    angle = np.arccos(angle)
-    return angle
 
-def generate_POSCARs(filename, lmax, amax, tempstart, tempincr, filestart, fileend, incr, num_iterations):
-    rad2deg = 180 / np.pi
 
-    natoms = np.genfromtxt(filename, dtype='i8', skip_header=6, max_rows=1)
-    scaling = np.genfromtxt(filename, dtype='f8', skip_header=1, max_rows=1)
-    basevect = np.genfromtxt(filename, dtype='f8', skip_header=2, max_rows=3)
-    ntype = np.genfromtxt(filename, dtype='<U11', skip_header=5, max_rows=1)
-    x, y, z = np.genfromtxt(filename, skip_header=8, unpack=True, max_rows=natoms, usecols=[0, 1, 2])
-    axis_matrix = np.array(basevect)
-    oldvol = np.linalg.det(axis_matrix)
-    v1 = basevect[:, 0]
-    v2 = basevect[:, 1]
-    v3 = basevect[:, 2]
+def create_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        
+def perturb_coordinates(coordinates,coordchange):
+    perturbed_coords = []
+    for coord in coordinates:
+        perturbed_coord = [num + random.uniform(-coordchange, coordchange) for num in coord]
+        perturbed_coords.append(perturbed_coord)
+    return perturbed_coords
+
+def perturb_lattice_vectors(vector,lmax,amax):
+
+    v1 = vector[0]
+    v2 = vector[1]
+    v3 = vector[2]
     p_a = np.linalg.norm(v1)
     p_b = np.linalg.norm(v2)
     p_c = np.linalg.norm(v3)
-    alpha = vec2angle(v2, v3)
-    beta = vec2angle(v1, v3)
-    gamma = vec2angle(v1, v2)
+    alpha = np.arccos(np.dot(v2, v3) / (p_b * p_c))
+    beta = np.arccos(np.dot(v1, v3) / (p_a * p_c))
+    gamma = np.arccos(np.dot(v1, v2) / (p_a * p_b))
 
     lx = p_a
     p_xy = p_b * np.cos(gamma)
@@ -35,105 +36,121 @@ def generate_POSCARs(filename, lmax, amax, tempstart, tempincr, filestart, filee
     p_yz = (p_b * p_c * (np.cos(alpha)) - p_xy * p_xz) / (ly)
 
     lz = np.sqrt(p_c ** 2 - p_xz ** 2 - p_yz ** 2)
-    cellMat_fixed = np.zeros((3, 3), dtype=np.double)
-    cellMat_fixed[0, 0] = lx
-    cellMat_fixed[0, 1] = 0.0
-    cellMat_fixed[0, 2] = 0.0
 
-    cellMat_fixed[1, 0] = p_xy
-    cellMat_fixed[1, 1] = ly
-    cellMat_fixed[1, 2] = 0.0
+    cellMat_randomstrain = np.zeros((3, 3), dtype=np.double)
+    cellMat_randomstrain[0, 0] = lx + lmax * (2 * np.random.random(1)[0] - 1.0)
+    cellMat_randomstrain[0, 1] = 0.0
+    cellMat_randomstrain[0, 2] = 0.0
 
-    cellMat_fixed[2, 0] = p_xz
-    cellMat_fixed[2, 1] = p_yz
-    cellMat_fixed[2, 2] = lz
+    cellMat_randomstrain[1, 0] = p_xy + amax * (2 * np.random.random(1)[0] - 1.0)
+    cellMat_randomstrain[1, 1] = ly + lmax * (2 * np.random.random(1)[0] - 1.0)
+    cellMat_randomstrain[1, 2] = 0.0
 
-    # Create directory for all-poscars
-    if os.path.exists("all-poscars"):
-        shutil.rmtree("all-poscars")
-    os.mkdir("all-poscars")
-    for k in range(num_iterations):
-        j = tempstart + tempincr * k
+    cellMat_randomstrain[2, 0] = p_xz + amax * (2 * np.random.random(1)[0] - 1.0)
+    cellMat_randomstrain[2, 1] = p_yz + amax * (2 * np.random.random(1)[0] - 1.0)
+    cellMat_randomstrain[2, 2] = lz + lmax * (2 * np.random.random(1)[0] - 1.0)
 
-        for i in range(filestart, fileend + 1):
-            dirname = f"input_{i}"
-            if os.path.exists(dirname):
-                shutil.rmtree(dirname)
-            os.mkdir(dirname)
-            shutil.copy("INCAR", dirname)
-            shutil.copy("KPOINTS", dirname)
-            # Generate new lattice parameters
-            delta_a = np.random.uniform(-lmax, lmax)
-            delta_b = np.random.uniform(-lmax, lmax)
-            delta_c = np.random.uniform(-lmax, lmax)
+    newvol = np.linalg.det(cellMat_randomstrain)
+    return cellMat_randomstrain, newvol
 
-            delta_alpha = np.random.uniform(-amax, amax)
-            delta_beta = np.random.uniform(-amax, amax)
-            delta_gamma = np.random.uniform(-amax, amax)
+def read_simulation_box(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    
+    lattice_vectors = [list(map(float, lines[i].split()[:3])) for i in range(2, 5)]
+    return lattice_vectors
 
-            lx_new = lx + delta_a
-            ly_new = ly + delta_b
-            lz_new = lz + delta_c
+def read_poscar(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
 
-            alpha_new = alpha + delta_alpha
-            beta_new = beta + delta_beta
-            gamma_new = gamma + delta_gamma
+    index_direct = lines.index("Direct\n")
+    num_atoms = int(lines[index_direct - 1].strip())
+    element_type = lines[index_direct - 2].strip()
+    coordinates = [list(map(float, line.split()[:3])) for line in lines[index_direct + 1: index_direct + 1 + num_atoms]]
 
-            v1_new = [lx_new, 0.0, 0.0]
-            v2_new = [ly_new * np.cos(gamma_new), ly_new * np.sin(gamma_new), 0.0]
-            v3_new = [
-                lz_new * np.cos(beta_new),
-                lz_new * (np.cos(alpha_new) - np.cos(beta_new) * np.cos(gamma_new)) / np.sin(gamma_new),
-                lz_new * np.sqrt(
-                    1.0
-                    - np.cos(beta_new) ** 2
-                    - ((np.cos(alpha_new) - np.cos(beta_new) * np.cos(gamma_new)) / np.sin(gamma_new)) ** 2
-                ),
-            ]
-            # Construct new lattice vectors matrix
-            axis_matrix_new = np.array([v1_new, v2_new, v3_new])
-            volume_new = np.linalg.det(axis_matrix_new)
+    return element_type, num_atoms, coordinates
 
-            scaling_factor = np.cbrt(oldvol / volume_new)
+def write_poscar(file_path, element_type, num_atoms, coordinates, lattice_vectors):
+    with open(file_path, 'w') as f:
+        f.write("Perturbed " + "\n" + "1.0" + "\n")
+        for vector in lattice_vectors:
+            f.write(f"{vector[0]:.9f} {vector[1]:.9f} {vector[2]:.9f}\n")
+        f.write(element_type + "\n")
+        f.write(str(num_atoms) + "\n")
+        f.write("Direct\n")
+        for coord in coordinates:
+            f.write(f"{coord[0]:.9f} {coord[1]:.9f} {coord[2]:.9f}\n")
 
-            # Scale the lattice vectors
-            axis_matrix_new_scaled = axis_matrix_new * scaling_factor
+if __name__ == "__main__":
+    input_file_path = "initial_poscar"  # Replace with the path to your input POSCAR file
+    coordchange_values = np.linspace(0.001, 0.05, 10)
+    lmax = 0.3
+    amax = 0.1
 
-            # Update the atomic positions
-            x_new = x * scaling_factor + j*(2*np.random.random(1)[0]-1.0)
-            y_new = y * scaling_factor + j*(2*np.random.random(1)[0]-1.0)
-            z_new = z * scaling_factor + j*(2*np.random.random(1)[0]-1.0)
+    element_type, num_atoms, coordinates = read_poscar(input_file_path)
+    lattice_vectors = read_simulation_box(input_file_path)
+
+    all_input_folder = "all_input"
+    create_folder(all_input_folder)
+
+    oldvol = np.linalg.det(lattice_vectors)
+
+    with open("volumechanges.txt", "w") as volume_file:
+        volume_file.write("CoordChange VolumeChange(%)\n")
+
+        for i, coordchange in enumerate(coordchange_values):
+            input_folder = f"input_{i:04d}"
+            create_folder(os.path.join(all_input_folder, input_folder))
+
+            perturbed_coordinates = perturb_coordinates(coordinates, coordchange)
+            perturbed_lattice_vectors, newvol = perturb_lattice_vectors(lattice_vectors, lmax, amax)
+
+            output_file_path = os.path.join(all_input_folder, input_folder, "Perturbed_POSCAR")
+            write_poscar(output_file_path, element_type, num_atoms, perturbed_coordinates, perturbed_lattice_vectors)
+
+            changevol = 100 * ((abs(newvol - oldvol)) / newvol)
+            volume_file.write(f"{coordchange:.6f} {changevol:.6f}\n")
 
 
-            # Write the modified POSCAR file
-            poscar_file = open(f"{dirname}/POSCAR", "w")
-            poscar_file.write("Modified POSCAR\n")
-            poscar_file.write("1.0\n")
-            np.savetxt(poscar_file, axis_matrix_new_scaled, fmt="%.16f", delimiter="   ", newline="\n")
-            poscar_file.write(f"{ntype}\n")
-            poscar_file.write(f"{natoms}\n")
-            poscar_file.write("Direct\n")
-            np.savetxt(poscar_file, np.column_stack((x_new, y_new, z_new)), fmt="%.16f", delimiter="   ", newline="\n")
-            poscar_file.write("\n")
-            poscar_file.close()
+if __name__ == "__main__":
+    input_file_path = "initial_poscar"  # Replace with the path to your input POSCAR file
+    coordchange_values = np.linspace(0.0001, 0.05, 1000)
+    lmax = 0.3
+    amax = 0.1
 
-            # Copy the POSCAR file to all-poscars directory
-            shutil.copy(f"{dirname}/POSCAR", f"all-poscars/POSCAR_{i}")
+    element_type, num_atoms, coordinates = read_poscar(input_file_path)
+    lattice_vectors = read_simulation_box(input_file_path)
 
-        filestart += incr
-        fileend += incr
+    all_input_folder = "all_input"
+    create_folder(all_input_folder)
 
-    print("All POSCAR files generated successfully.")
+    # Create a folder for storing the copied POSCAR files
+    poscars_folder = os.path.join(all_input_folder, "all_POSCARS")
+    create_folder(poscars_folder)
 
-# usage
-filename = 'replicated_POSCAR'
-lmax = 0.2  # EACH length change in between +-0.1
-amax = 0.05  # EACH angle change in between 2 degrees from ideal
-tempstart = 0.008
-tempincr = 0.005
-filestart = 1
-fileend = 100
-incr = 100
-num_iterations = 5 # 5*100 = 500 files
+    oldvol = np.linalg.det(lattice_vectors)
 
-generate_POSCARs(filename, lmax, amax, tempstart, tempincr, filestart, fileend, incr, num_iterations)    
+    with open("volumechanges.txt", "w") as volume_file:
+        volume_file.write("CoordChange VolumeChange(%)\n")
+
+        for i, coordchange in enumerate(coordchange_values):
+            input_folder = f"input_{i:04d}"
+            create_folder(os.path.join(all_input_folder, input_folder))
+
+            perturbed_coordinates = perturb_coordinates(coordinates, coordchange)
+            perturbed_lattice_vectors, newvol = perturb_lattice_vectors(lattice_vectors, lmax, amax)
+
+            output_file_path = os.path.join(all_input_folder, input_folder, "POSCAR")
+            write_poscar(output_file_path, element_type, num_atoms, perturbed_coordinates, perturbed_lattice_vectors)
+
+            changevol = 100 * ((abs(newvol - oldvol)) / newvol)
+            volume_file.write(f"{coordchange:.6f} {changevol:.6f}\n")
+
+            # Copy the Perturbed_POSCAR file to the "all_POSCARS" folder with a unique identifier
+            unique_file_name = f"POSCAR_{i:04d}"
+            shutil.copy(output_file_path, os.path.join(poscars_folder, unique_file_name))
+            
+            # Copy INCAR and KPOINTS files to the "input_{i:04d}" folder
+            shutil.copy("INCAR", os.path.join(all_input_folder, input_folder, "INCAR"))
+            shutil.copy("KPOINTS", os.path.join(all_input_folder, input_folder, "KPOINTS"))
